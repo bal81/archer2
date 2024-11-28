d44b2bb6645062cbe00f87714e4a65952c881e8f
balder <b.villagomez@qub.ac.uk>
2024-11-28 10:07:54 +0000

    micro services

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -4,9 +4,17 @@
 import glob
 import os
 import numpy as np
+import sys
+sys.path.append("C:\\Users\\3048844\\OneDrive - Queen's University Belfast\\Documents\\cursos\\archer4\\archer2\\")
 
 from inflammation import models, views
 
+def load_inflammation_data(data_dir):
+    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+    data = map(models.load_csv, data_file_paths)
+    return data
 
 def analyse_data(data_dir):
     """Calculates the standard deviation by day between datasets.
@@ -14,12 +22,7 @@ def analyse_data(data_dir):
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
-
-
+    data= load_inflammation_data(data_dir)
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
 
